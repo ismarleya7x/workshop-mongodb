3cc8c2949e106f179f89ba6d68c3601a7e34a814
IsmarleyA7X <ismarley_2005@msn.com>
2020-09-25 19:47:57 -0300

    Inserção de usuário

--- src/main/java/com/ismarleycarvalho/workshopmongodb/services/UserService.java
+++ src/main/java/com/ismarleycarvalho/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ismarleycarvalho.workshopmongodb.domain.User;
+import com.ismarleycarvalho.workshopmongodb.dto.UserDTO;
 import com.ismarleycarvalho.workshopmongodb.repository.UserRepository;
 import com.ismarleycarvalho.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }

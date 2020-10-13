f2eb734f78532a132886419006b74d0cd8f4320e
IsmarleyA7X <ismarley_2005@msn.com>
2020-10-13 18:20:57 -0300

    Obtendo post por ID

---
+++ src/main/java/com/ismarleycarvalho/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.ismarleycarvalho.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ismarleycarvalho.workshopmongodb.domain.Post;
+import com.ismarleycarvalho.workshopmongodb.repository.PostRepository;
+import com.ismarleycarvalho.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado!"));
+	}
+}
